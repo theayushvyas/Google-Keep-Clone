a48a4662e76b689e643e03cd133bb16dc798aead
Ayush vyas <ayush.manuvyas@gmail.com>
2020-09-15 17:30:51 +0530

    Updates

---
+++ precache-manifest.928b2d9df03a2aaef3d239dd514f6468.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ed6261db189e16ccf4e74bf32469fd91",
+    "url": "/Keep/index.html"
+  },
+  {
+    "revision": "3ea259af8555607d7973",
+    "url": "/Keep/static/css/main.07ba515a.chunk.css"
+  },
+  {
+    "revision": "e62c1f8772dc538b8302",
+    "url": "/Keep/static/js/2.b6d3dbd9.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/Keep/static/js/2.b6d3dbd9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3ea259af8555607d7973",
+    "url": "/Keep/static/js/main.d1f9a65a.chunk.js"
+  },
+  {
+    "revision": "9e9cfb079353de932d07",
+    "url": "/Keep/static/js/runtime-main.0093d96f.js"
+  }
+]);
